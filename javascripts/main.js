59c5133d8530ee27143b1c225a45596ccf06a091
racheliff <racheliff@gmail.com>
2015-11-09 00:02:12 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,9 @@
 
 angular.module('MyApp', [])
 
-    .controller('DemoCtrl', function ($scope, SimpleGithubUser, AdvancedGithubUser) {
-
-    $scope.users = [];
+    .controller('DemoCtrl', function ($scope){//, SimpleGithubUser, AdvancedGithubUser) {
+        $scope.name = 'Racheli';
+ /*   $scope.users = [];
 
     $scope.fetchUsers = function () {
         $scope.users = [];
@@ -16,12 +16,12 @@ angular.module('MyApp', [])
                 $scope.users.push(user);
             });
         });
-    }
+    }*/
 })
-
-    .factory('SimpleGithubUser', function ($http) {
+/*
+.factory('SimpleGithubUser', function ($http) {
     var apiUrl = 'https://api.github.com/';
-
+    
     // instantiate our object
     var SimpleGithubUser = function (username) {
         this.username = username;
@@ -29,7 +29,7 @@ angular.module('MyApp', [])
     };
     // this method will fetch data from GH API and return a promise
     SimpleGithubUser.prototype.getProfile = function () {
-        // Generally, javascript callbacks, like here the $http.get callback, change the value of the "this" variable inside callbacks so we need ot keep a reference to the current instance "this", and we do it with the following :
+    // Generally, javascript callbacks, like here the $http.get callback, change the value of the "this" variable inside callbacks so we need ot keep a reference to the current instance "this", and we do it with the following :
         var self = this;
         return $http.get(apiUrl + 'users/' + this.username).then(function (response) {
             // we store the API result in user.profile. 
@@ -74,3 +74,4 @@ angular.module('MyApp', [])
     };
     return AdvancedGithubUser;
 })
+*/
