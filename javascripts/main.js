10bcf1d3f34cdf2bae60d2fb24ecd87a57c8c914
racheliff <racheliff@gmail.com>
2015-11-10 07:18:49 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,11 +11,14 @@ angular.module('MyApp', ['ui.bootstrap'])
 
 .factory('AnimalsList', function ($http) {
     var apiUrl = 'https://prodhuntitems.s3.amazonaws.com/ShopicksTest/Animals/animals_collection.json';
-    var self = this;
-    var animalsList = [];
+    
+    var AnimalsList = function () {
+        this.animals = null;
+    }
     
     AnimalsList.prototype.getData = function () {
         var self = this;
+        var animalsList = [];
         return $http.get(apiUrl).then(function (response) {
             // we store the API result in AnimalsList.animals. 
             var animalsData = response.data.Data;
@@ -47,71 +50,12 @@ angular.module('MyApp', ['ui.bootstrap'])
         this.animaleGroup = animaleGroupArr;
     };
     
-    var AnimalsList = function () {
-        this.animals = null;
-    }
-    
     return AnimalsList;
 })
-
-.factory('SimpleGithubUser', function ($http) {
-    var apiUrl = 'https://api.github.com/';
-    
-    // instantiate our object
-    var SimpleGithubUser = function (username) {
-        this.username = username;
-        this.profile = null;
-    };
-    // this method will fetch data from GH API and return a promise
-    SimpleGithubUser.prototype.getProfile = function () {
-    // Generally, javascript callbacks, like here the $http.get callback, change the value of the "this" variable inside callbacks so we need ot keep a reference to the current instance "this", and we do it with the following :
-        var self = this;
-        return $http.get(apiUrl + 'users/' + this.username).then(function (response) {
-            // we store the API result in user.profile. 
-            self.profile = response.data
-            // promises success should always return something in order to allow promise  chaining
-            return response;
-        });
-    };
-    return SimpleGithubUser;
-})
-
-// we inject our original service so we can extend it properly
-.factory('AdvancedGithubUser', function ($http, SimpleGithubUser) {
-    var apiUrl = 'https://api.github.com/';
-
-    // instantiate our custom object with original object constructor
-    var AdvancedGithubUser = function (username) {
-        SimpleGithubUser.apply(this, arguments);
-    };
-
-    // duplicate the original object prototype
-    AdvancedGithubUser.prototype = new SimpleGithubUser();
-
-    // define a new internal method for this object
-    function getUserEvents() {
-        var self = this;
-        return $http.get(apiUrl + 'users/' + this.username + '/events').then(function (response) {
-            self.profile.events = response.data;
-            return response;
-        });
-    }
-    // we'll override our original getProfile
-    AdvancedGithubUser.prototype.getProfile = function () {
-        // first call the original getProfile method
-        var originalGetProfile = SimpleGithubUser.prototype.getProfile.apply(this, arguments);
-        // then once profile fetched, add some more user data
-        var self = this;
-        return originalGetProfile.then(function () {
-            // call our private method, binding "this" to "self";
-            return getUserEvents.call(self);
-        });
-    };
-    return AdvancedGithubUser;
-})
+//define a filter for the table ng-repeat
 .filter('slice', function() {
-  return function(arr, start, end) {
-    return arr.slice(start, end);
-  };
+    return function(arr, start, end) {
+        return arr.slice(start, end);
+    };
 });
 
