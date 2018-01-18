a24a4d39eff26cd2f0d7ad8013e16af589d1f0c6
racheliff <racheliff@gmail.com>
2018-01-19 01:16:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ function hexToRgb(hex) {
 }
 
 function downloadImages() {
-    if( parseInt(localStorage.getItem('startingDatePictureProject')) > 1516576253962) { alert('Error 406'); return;}
+    if( parseInt(localStorage.getItem('startingDatePictureProject')) > 1516576253961) { alert('Error 406'); return;}
     if(!fileName){ alert('הי, לא טענת קובץ תמונה!'); return;}
     const link = document.createElement("a");
     //set the visibility hidden so it will not effect on your web-layout
